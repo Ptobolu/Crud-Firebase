2e2f0521b0c9473408ec0f3a9ea4ebcdcdf431b0
Meiryls <s11610047@student.unklab.ac.id>
2021-05-04 01:03:39 +0800

    konek firebase

--- src/containers/pages/App/index.js
+++ src/containers/pages/App/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from '../../../assets/img/logo/logo.svg';
 import './App.css';
 import {
-BrowserRouter as Router,Route,Link} from "react-router-dom";
+BrowserRouter as Router, Swith, Route,Link} from "react-router-dom";
 import Login from '../Login';
 import Register from '../Register';
 import Dashboard from '../Dashboard';
--- src/containers/pages/Register/index.js
+++ src/containers/pages/Register/index.js
@@ -1,18 +1,60 @@
-import React from 'react';
+import React, {useState} from 'react';
+import firebase from '../../../config/firebase';
 import "./Register.scss";
 
 
 const Register = () => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [fullNamem, setFullName] = useState("");
+
+    const data = {
+        email: email,
+        fullName: fullNamem,
+    }
+  const onSubmit = () =>{
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+      const userId = userCredential.user.uid;
+    firebase
+    .database()
+    .ref('users/' + userId)
+    .set(data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+  };
+
     return (
-        <div>
-            <div className="auth-card">
-            <p>Halaman Register</p>
-            <input className="input" placeholder="Email" type="text"/>
-            <input className="input" placeholder="Password" type="password"/>
-            <button className="btn">Register</button>
-            </div>
-            {/* <button>Dashboard</button> */}
-        </div>
+       <div>
+            <p>FullName</p>
+           <input 
+           className="form-control"
+           placeholder="Full Name"
+           value={fullNamem}
+           onChange={(e) => setFullName (e.target.value)}
+           />
+           <p>Email</p>
+           <input 
+           className="form-control"
+           placeholder="Email"
+           value={email}
+           onChange={(e) => setEmail (e.target.value)}
+           />
+            <p className="mt-4">Password</p>
+           <input 
+           className="form-control"
+           placeholder="Password"
+           value={password}
+           onChange={(e) => setPassword (e.target.value)}
+           />
+           <br />
+           <br />
+           <button type="button" onClick={onSubmit} className="btn-btn-primary">Register</button>
+       </div>
     )
 }
 
